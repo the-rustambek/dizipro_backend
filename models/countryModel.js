695588072254768969ccd769b810f30d99168842
Rustambek Mamadaliyev <80699038+Rustambek-Mamadaliyev0301@users.noreply.github.com>
2021-11-26 17:20:44 +0500

    add countryModel

--- models/countryModel.js
+++ models/countryModel.js
@@ -2,7 +2,32 @@ module.exports = async (sequelize, Sequelize)=>{
     return await sequelize.define('countries',{
 
         country_id:{
-            type: Sequelize.
-        }
+            type: Sequelize.INTEGER,
+            allowNull:false
+        },
+        country_iso:{
+           type: Sequelize.STRING(2),
+           allowNull:false
+        },
+        country_name:{
+            type:Sequelize.STRING(32),
+            allowNull:false,
+        },
+        country_nicename:{
+            type:Sequelize.STRING(32),
+            allowNull:false,
+        },
+        country_iso3:{
+            type:Sequelize.STRING(3),
+            allowNull:false,
+        },
+        country_num_code:{
+            type:Sequelize.STRING(3),
+            allowNull:false,
+        },
+        country_phone_code:{
+            type:Sequelize.INTEGER,
+            allowNull:false,
+        },
     })
 }
