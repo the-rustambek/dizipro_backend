130dbad596880bbfd95b102d2596d47296644430
Rustambek Mamadaliyev <80699038+Rustambek-Mamadaliyev0301@users.noreply.github.com>
2021-11-26 09:30:42 +0500

    add customerrror

---
+++ helpers/customError.js
@@ -0,0 +1,15 @@
+module.exports.customError =  class customError extends Error {
+    constructor(code, message){
+        super(message);
+        this.code = code;
+    }
+}
+
+module.exports.errorHandlerMiddleware= function errorHandlerMiddleware(
+    error, req, res, next
+){
+    res.status (error.code || 500).json({
+        ok: false,
+        message: error.message || "Internal Server Error",
+    })
+}
