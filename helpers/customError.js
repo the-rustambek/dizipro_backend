b5d5beced68c94033efeb6bdd381dee540497e83
Rustambek Mamadaliyev <80699038+Rustambek-Mamadaliyev0301@users.noreply.github.com>
2021-11-26 09:39:52 +0500

    add errorHandleMiddleware

--- helpers/customError.js
+++ helpers/customError.js
@@ -1,14 +1,14 @@
-module.exports.customError =  class customError extends Error {
-    constructor(code, message){
+module.exports.customError = class customError extends Error {
+    constructor(code, message) {
         super(message);
         this.code = code;
     }
 }
 
-module.exports.errorHandlerMiddleware= function errorHandlerMiddleware(
+module.exports.errorHandlerMiddleware = function errorHandlerMiddleware(
     error, req, res, next
-){
-    res.status (error.code || 500).json({
+) {
+    res.status(error.code || 500).json({
         ok: false,
         message: error.message || "Internal Server Error",
     })
