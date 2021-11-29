d978b3bd660d7abe54c6615aa7e390f2fbb80756
Rustambek Mamadaliyev <80699038+Rustambek-Mamadaliyev0301@users.noreply.github.com>
2021-11-29 15:18:57 +0500

    add userValidation

---
+++ validations/userValidation.js
@@ -0,0 +1,13 @@
+const Joi = require("joi")
+
+module.exports = class userValidations {
+    static async userCreateAccountValidation(data, customError) {
+        return await Joi.object({
+            user_name: Joi.string().min(2).max(64).required().error(new customError(400, "Name is invalid")),
+            user_email: Joi.string().email().required().lowercase().error(new customError(400, "Email is invalid")),
+            user_gender: Joi.string().valid("male","female").required().error(new customError(400, "Gender is invalid")),
+            user_password: Joi.string().min(4).required().error(new customError(400, "Password is invalid")),
+            country_id: Joi.number().required().error(new customError(400, "Country id is invalid")),
+        }).validateAsync(data);
+    }
+}
