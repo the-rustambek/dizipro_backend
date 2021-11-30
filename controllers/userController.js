00c2a079e05a565bde157a1f60a57e2a41203f48
Rustambek Mamadaliyev <80699038+Rustambek-Mamadaliyev0301@users.noreply.github.com>
2021-11-30 15:37:35 +0500

    add userLoginAccountPostController

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,5 +1,6 @@
 const {
-    generateCrypt
+    generateCrypt,
+    compareCrypt
 } = require("../modules/bcrypt")
 const userValidations = require("../validations/userValidation")
 module.exports = class userController {
@@ -10,7 +11,7 @@ module.exports = class userController {
 
             const user = await req.db.users.create({
                 ...data,
-                user_password:generateCrypt(data.user_password),
+                user_password: generateCrypt(data.user_password),
             });
             // console.log(user);
 
@@ -20,17 +21,17 @@ module.exports = class userController {
                 user_id: user_dataValues.user_id,
             });
 
-            
-			const token = createToken({
-				session_id: session.dataValues.session_id,
-				role: "user",
-			});
+
+            const token = createToken({
+                session_id: session.dataValues.session_id,
+                role: "user",
+            });
 
 
-            res.status(201).json({
-                ok:true,
-                message:"User created successfully",
-                data:{
+            await res.status(201).json({
+                ok: true,
+                message: "User created successfully",
+                data: {
 
                 }
             })
@@ -48,4 +49,56 @@ module.exports = class userController {
             next(error);
         }
     }
+
+
+
+    static async userLoginAccountPostController(req, res, next) {
+        try {
+            const data = await userValidations.userLoginAccountValidation(req.body, res.error);
+
+            const user = await req.db.users.findOne({
+                where: {
+                    user_email: data.user_email,
+                },
+                raw: true,
+            });
+
+            if (!user) throw new res.error(404, "User not found");
+
+            const isTrust = compareCrypt(data.user_password, user.user_password);
+
+            if (!isTrust) throw new res.error(400, "Password is incorrect");
+
+            await req.db.sessions.destroy({
+                where: {
+                    session_user_agent: req.headers["user_agent"] || "Unknown",
+                    user_id: user_id
+                }
+            });
+
+            const session = await req.db.sessions.create({
+                session_user_agent: req.headers["user_agent"] || "Unknown",
+                user_id: user_id
+
+            })
+
+            const token = createToken({
+                session_id: session.dataValues.session_id,
+            });
+
+            res.status(201).json({
+                ok: true,
+                message: "Logged successfully",
+                data: {
+                    token
+                }
+            })
+        } catch (error) {
+            next(error)
+
+        }
+    }
+
+
+
 }
